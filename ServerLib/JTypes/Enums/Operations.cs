28814ab700053f1f6de344efd49a4733f4470cfd
Igor Grigoryev <i.grigoryev@massaccess.net>
2019-01-28 18:37:57 +0300

    Добавлены операции над объектами системы

---
+++ ServerLib/JTypes/Enums/Operations.cs
@@ -0,0 +1,28 @@
+﻿namespace ServerLib.JTypes.Enums
+{
+    /// <summary>
+    /// Операции над объектами
+    /// </summary>
+    public enum Operations
+    {
+        /// <summary>
+        /// Чтение
+        /// </summary>
+        read,
+
+        /// <summary>
+        /// Создание
+        /// </summary>
+        add,
+
+        /// <summary>
+        /// Изменение
+        /// </summary>
+        edit,
+
+        /// <summary>
+        /// Удаление
+        /// </summary>
+        delete
+    }
+}
---
+++ bcsserver/Handlers/HandlerRolesObjectsClass.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using CLProject;
+using Newtonsoft.Json;
+using System.Collections.Concurrent;
+using ServerLib.JTypes.Enums;
+
+namespace bcsserver.Handlers
+{
+    public class HandlerRolesObjectsClass : HandlerBaseClass
+    {
+        /// <summary>
+        /// Словарь должностей пользователей
+        /// </summary>
+        private readonly ConcurrentDictionary<long, ServerLib.JTypes.Server.JobClass> JobsCollection;
+
+        public HandlerRolesObjectsClass(UserSessionClass AUserSession) : base(AUserSession)
+        {
+            JobsCollection = new ConcurrentDictionary<long, ServerLib.JTypes.Server.JobClass>();
+        }
+    }
+}
